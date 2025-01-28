bb28e7851582518dd81ed9d895ea5cc4450efa2a
Fozle Rabbi <fozlerabbishuvo@gmail.com>
2025-01-28 16:16:25 +0600

    remove

--- src/server.ts
+++ src/server.ts
@@ -27,7 +27,6 @@ if (cluster.isPrimary) {
       const messageService = new MessageService(io, MessageModel);
 
       io.on('connection', (socket) => {
-        console.log('A user connected: ' + socket.id);
 
         socket.on('join', (username) => messageService.handleJoin(socket, username));
         socket.on('message', (messageData) => messageService.handleMessage(messageData));
