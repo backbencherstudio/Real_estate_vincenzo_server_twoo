ba7c4ddd9790ab6c940852e6fdd9c251b0e7207c
Fozle Rabbi <fozlerabbishuvo@gmail.com>
2025-02-17 12:06:11 +0600

    update in app.ts

--- src/app.ts
+++ src/app.ts
@@ -43,7 +43,7 @@ app.use((req, res, next) => {
 
 app.use(
   cors({
-    origin: ['http://localhost:5173'],  
+    origin: ['http://localhost:5173', 'https://rentpadhomes.com'],  
     credentials: true,
   })
 );
