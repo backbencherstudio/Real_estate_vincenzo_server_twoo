e6c4f02adc6f27ed120f8a8b0ec6ec5048176261
Fozle Rabbi <fozlerabbishuvo@gmail.com>
2025-03-01 09:58:25 +0600

    update

--- src/app/Modules/admin/admin.service.ts
+++ src/app/Modules/admin/admin.service.ts
@@ -211,8 +211,6 @@ const getAllDataOverviewByAdminFromDB = async (selectedDate: string): Promise<Ov
         const startDate = new Date(year, month - 1, 1);
         const endDate = new Date(year, month, 0, 23, 59, 59, 999); 
 
-        console.log(selectedDate);
-
         const queries = [
             {
                 key: "propertyLength",
